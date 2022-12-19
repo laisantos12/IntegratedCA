0da9a553c72d506c6489cb6321c6aeafd9247e0c
Tamiris <62302649+tahrodriguess@users.noreply.github.com>
2022-12-19 18:30:58 +0000

    update SuggestedMovies

--- src/integratedca/SuggestedMovies.java
+++ src/integratedca/SuggestedMovies.java
@@ -4,10 +4,27 @@
  */
 package integratedca;
 
+
+import java.util.List;
+import java.util.Random;
+
 /**
  *
  * @author tahro
  */
 public class SuggestedMovies {
+    List<Movie> suggestions = newArrayList<>();
+    Random randomNum = new Random();
+    int nuumberOfSuggestions = 5;
     
+    public List<Movie> getSuggestions() {
+        return suggestions;
+    }
+    
+    public void suggestedMovies(List<Movie> movies, int sizeOfTheMovieList){
+        for(int i=0;i<numberOfSuggestions;i++){
+        suggestions.add(movies.get(randomNum.nextInt(sizeOfTheMovieList)));
+
+    }
+
 }
