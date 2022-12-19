60d71828e087590465dc71612a86b107d6e24586
Lais Santos <lsantospersonal@hotmail.com>
2022-12-19 19:46:34 +0000

    fixed

--- src/integratedca/RentMovies.java
+++ src/integratedca/RentMovies.java
@@ -17,52 +17,54 @@ public class RentMovies {
     private String personRenting;
     private int backDate;
 
-//    public RentMovies(int id, int date, int rentedMovieID, String personRenting, int backDate) {
-//        this.id = id;
-//        this.date = date;
-//        this.rentedMovieID = rentedMovieID;
-//        this.personRenting = personRenting;
-//        this.backDate = backDate;
-//    }
+    // public RentMovies(int id, int date, int rentedMovieID, String personRenting,
+    // int backDate) {
+    // this.id = id;
+    // this.date = date;
+    // this.rentedMovieID = rentedMovieID;
+    // this.personRenting = personRenting;
+    // this.backDate = backDate;
+    // }
     Scanner input = new Scanner(System.in);
-    
+
     MovieListCreator movieList = new MovieListCreator();
-    
-    RentedMovieList rentedMovieList= new RentedMovieList();
-    
+
+    RentedMovieList rentedMovieList = new RentedMovieList();
+
     SuggestedMovies suggestions = new SuggestedMovies();
-    
-    
-     public void rentMovie() throws IOException{
-        do{
+
+    public void rentMovie() throws IOException {
+        do {
             movieList.createListFrom("src/moviesmetadataedited.csv");
             System.out.println("\nWelcome to EirVid - From Your RTE Player."
-                + "\nPlease Select:"
-                + "\n1 - Rent a Movie"
-                + "\n2 - My Renteded Movies"
-                + "\n3 - Recomendations"
-                + "\n4 - Exit");
-        
-        switch(input.nextInt()){
-            case 1 : {
-            
-                System.out.println(movieList.getMovies());
-                System.out.println("Select the ID of the movie you would like to rent");
-                rentedMovieList.addMovieToRentedList(movieList.getMovies().get(input.nextInt()));
-                break;
+                    + "\nPlease Select:"
+                    + "\n1 - Rent a Movie"
+                    + "\n2 - My Renteded Movies"
+                    + "\n3 - Recomendations"
+                    + "\n4 - Exit");
+
+            switch (input.nextInt()) {
+                case 1: {
+
+                    System.out.println(movieList.getMovies());
+                    System.out.println("Select the ID of the movie you would like to rent");
+                    rentedMovieList.addMovieToRentedList(movieList.getMovies().get(input.nextInt()));
+                    break;
+                }
+                case 2: {
+                    System.out.println(rentedMovieList.getRentedMovies());
+                    break;
+                }
+                case 3: {
+                    suggestions.suggestedMovies(movieList.getMovies(), movieList.numberOfMovies());
+                    System.out.println(suggestions.getSuggestions());
+                }
+
             }
-            case 2: {System.out.println(rentedMovieList.getRentedMovies());
-            }
-            case 3:{
-                suggestions.suggestedMovies(movieList.getMovies(), movieList.numberOfMovies());
-                System.out.println(suggestions.getSuggestions());
-            }
-            
-        }
-            
-        
-      }while(true);
-     }
+
+        } while (true);
+    }
+
     private static final int RENTDURATION = 60000;
 
     private long activatedAt = Long.MAX_VALUE;
